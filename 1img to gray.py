014552c0a19c2fe2f5a864953eeb10b10902b917
AK.Sathish Kumar <112234185+192125061@users.noreply.github.com>
2023-08-08 14:03:43 +0530

    Update 1img to gray.py

--- 1img to gray.py
+++ 1img to gray.py
@@ -1,7 +1,1 @@
-import cv2
-import numpy as np
-path = "E:/computer_vision/test.png"
-img = cv2.imread(path)
-imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-cv2.imshow("GrayScale",imgGray)
-cv2.waitKey(0)
+
